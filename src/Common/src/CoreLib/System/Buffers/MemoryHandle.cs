1e8083be27510393f83644c649dfac85461f0c25
Marek Safar <marek.safar@gmail.com>
2018-10-05 19:14:51 +0200

    Hide more span APIs

--- src/Common/src/CoreLib/System/Buffers/MemoryHandle.cs
+++ src/Common/src/CoreLib/System/Buffers/MemoryHandle.cs
@@ -10,7 +10,7 @@ namespace System.Buffers
     /// <summary>
     /// A handle for the memory.
     /// </summary>
-    public unsafe struct MemoryHandle : IDisposable
+    internal unsafe struct MemoryHandle : IDisposable
     {
         private IRetainable _retainable;
         private void* _pointer;
